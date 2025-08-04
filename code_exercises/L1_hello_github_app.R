c16eb18d685ad4ba538a56aae1b89460c000475f
Sriya-235 <vsriya23@gmail.com>
2025-08-04 12:01:31 -0500

    restore github exercise

--- code_exercises/L1_hello_github_app.R
+++ code_exercises/L1_hello_github_app.R
@@ -50,7 +50,7 @@ server <- function(input, output) {
     
     ## TODO-1: Reformat base old faithful histogram to ggplot code
     
-    ggplot(faithful, aes(x = waiting)) + geom_histogram(bins = input$bins) + theme_classic() + labs(x = 'A better X axis title') + title("A better plot title")
+    ## ggplot(faithful, aes(x = waiting)) + geom_histogram(bins = input$bins) 
     
     
     ## Bonus TODO: Make the plot prettier!
